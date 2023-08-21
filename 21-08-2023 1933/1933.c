86950fa8b267a81ab9a8bf83157c7b758b1e198a
Mateus Maciel <matteus6537@hotmail.com>
2023-08-21 15:11:17 -0300

    ADD new code

---
+++ 21-08-2023 1933/1933.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b, c;
+
+    scanf("%d %d", &a, &b);
+
+    if (a == b) {
+        c = a;
+    } else {
+        if (a > b) {
+            c = a;
+        } else {
+            c = b;
+        }
+    }
+
+    printf("%d\n", c);
+
+ 
+    return 0;
+}
