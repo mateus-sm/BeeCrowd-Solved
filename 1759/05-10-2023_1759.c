8f83caee847692f1b85faf1a7f4ee20b359fc445
Mateus Maciel <matteus6537@hotmail.com>
2023-10-05 19:04:44 -0300

    ADD new code

---
+++ 1759/05-10-2023_1759.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int contador;
+
+    scanf("%d", &contador);
+
+    while (contador > 0) {
+        
+        if (contador == 1) {
+            printf("Ho");
+        } else {
+            printf("Ho ");
+        }
+        
+        contador--;
+    }
+ 
+    
+    printf("!\n");
+
+    return 0;
+}
