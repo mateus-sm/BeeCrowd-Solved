4a27807a65c6bf6e1033f7287baa6650d82245c6
Mateus Maciel <matteus6537@hotmail.com>
2023-10-10 19:15:40 -0300

    ADD new code

--- Iniciante/1001/27-09-2023_1001.py
+++ Iniciante/1001/27-09-2023_1001.py
@@ -1,4 +1,6 @@
 nota1 = int(input())
 nota2 = int(input())
+
 soma = nota1 + nota2
+
 print("X = %d"%soma)
---
+++ Iniciante/1011/1011.py
@@ -0,0 +1,6 @@
+pi = 3.14159
+r = float(input())
+
+volume = (4.0/3) * pi * (r * r * r)
+
+print("VOLUME = %.3lf" %volume)
