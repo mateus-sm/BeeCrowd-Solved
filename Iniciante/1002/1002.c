d7b4ce3899771b7127ebc6fcabd56dd498f5b52d
Mateus Maciel <matteus6537@hotmail.com>
2023-10-10 12:23:14 -0300

    ADD new code

---
+++ Iniciante/1002/1002.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    double A, R;
+    scanf("%lf", &R);
+    A = R * R * 3.14159;
+    printf("A=%.4lf\n", A);
+    return 0;
+}
---
+++ Iniciante/1002/28-09-2023_1002.py
@@ -0,0 +1,7 @@
+import math
+n = 3.14159
+raio = input()
+raio = float(raio)
+area = n * math.pow(raio, 2)
+
+print("A=%.4f" %area)
