c5ae6e64bce7067715b3a9dab11bff4a8b4a5038
Mateus Maciel <matteus6537@hotmail.com>
2023-11-23 19:14:22 -0300

    UPDATE 1241.c

--- Strings/Pagina 01/1241.c
+++ Strings/Pagina 01/1241.c
@@ -1,55 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 #define TF 1000
- 
+
 void repeticao() {
-    int vet1[TF], vet2[TF];
-    char str[TF];
+    char str1[TF], str2[TF];
+    char strAux[TF];
     int TL1 = 0, TL2 = 0;
     int i = 0, j = 0, k = 0;
 
-    gets(str);
+    scanf("%s",&str1);
+    scanf("%s",&str2);
 
-    while (i < strlen(str) && str[i] != ' ') {
-        vet1[TL1] = str[i] - '0';  // Converte caractere para inteiro
-        i++;
-        TL1++;
-    }
-    i++;  // Ignora o espaço
-    while (i < strlen(str)) {
-        vet2[TL2] = str[i] - '0';  // Converte caractere para inteiro
-        i++;
-        TL2++;
+    if (strlen(str1) < strlen(str2)) {
+        strcpy(strAux,str1);
+        strcpy(str1,str2);
+        strcpy(str2,strAux);
     }
 
+    TL1 = strlen(str1);
+    TL2 = strlen(str2);
 
+    k = 0;
     for (i = TL1 - 1, j = TL2 - 1; j >= 0; i--, j--) {
-        if (vet2[j] == vet1[i]) {
+        if (str2[j] == str1[i]) {
             k++;
         } else {
-            break; // If a mismatch is found, break out of the loop
+            break;
         }
     }
 
+    
     if (k == TL2) {
         printf("encaixa\n");
     } else {
         printf("nao encaixa\n");
     }
-
+    
 }
 
-void casos() {
+int main() {
     int numcasos, i;
+
     scanf("%d", &numcasos);
     fflush(stdin);
 
     for (i = 0; i < numcasos; i++) {
         repeticao();
     }
-}
 
-int main() {
-    casos();
     return 0;
 }
