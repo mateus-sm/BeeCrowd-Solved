a3257ee5b02b8060adba7472fb5b2224f3aa3124
Mateus Maciel <matteus6537@hotmail.com>
2025-02-02 19:56:38 -0300

    UPDATE 1557.c

--- Iniciante/Pagina 05/1557/1557.c
+++ Iniciante/Pagina 05/1557/1557.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+int size(int);
+
 int main(void) {
     int TL;
 
@@ -9,16 +11,21 @@ int main(void) {
     while(TL != 0) {
         int mat[TL][TL];
 
+        //popula matriz
         for (size_t linha = 0; linha < TL; linha++) {
             for (size_t coluna = 0; coluna < TL; coluna++) {
                 mat[linha][coluna] = pow(2, coluna + linha);
             }
         }
-        
+
+        int tamanhoDigito = size(mat[TL - 1][TL - 1]) + 1;
+        //printf("Tamanho de %d eh igual a: %d\n", mat[TL - 1][TL - 1], tamanhoDigito);
+
         for(size_t linha = 0; linha < TL; linha++) {
-            printf("%3.d", mat[linha][0]);
+            //imprimir a primeira coluna com um espaço a menos, simplesmente porque sim.
+            printf("%*d", tamanhoDigito - 1, mat[linha][0]);
             for(size_t coluna = 1; coluna < TL; coluna++) {
-                printf("%4.d", mat[linha][coluna]);
+                printf("%*d", tamanhoDigito, mat[linha][coluna]);
             }
             puts("");
         }
@@ -28,4 +35,16 @@ int main(void) {
     }
 
     return 0;
+}
+
+int size(int num) {
+    int aux = num;
+    int tamanho = 0;
+
+    while (aux != 0) {
+        aux = aux / 10;
+        tamanho++;
+    }
+
+    return tamanho;
 }
